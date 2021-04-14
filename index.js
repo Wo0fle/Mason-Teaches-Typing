465e0e3ea9c8a5580b86176a91aed0dfd4bbdd3f
Seby Amador <81187812+Wo0fle@users.noreply.github.com>
2021-04-13 21:36:43 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+const prompt = require('prompt-sync')();
+
+console.log("helo my name is mason");
+console.log("i will teach you to type good");
+var username = prompt ("what is ur name ");
+console.log("hi " + username);
+console.log("ok so put in a word any word");
+var word = prompt("word: ");
+console.log(word + "...");
+console.log("perfect good job");
